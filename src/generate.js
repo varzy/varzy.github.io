3889172391eb8a1e00057875d0b3522cc54f4715
zy <varzy@live.com>
2021-02-05 22:47:04 +0800

    update layout

--- src/generate.js
+++ src/generate.js
@@ -1,51 +1,15 @@
-const fs = require("fs");
-const path = require("path");
-const pug = require("pug");
+const fs = require('fs');
+const path = require('path');
+const pug = require('pug');
+const locales = require('./locales');
 
-const renderFn = pug.compileFile(path.resolve(__dirname, "./templates/index.pug"), {
+const renderFn = pug.compileFile(path.resolve(__dirname, './templates/index.pug'), {
   pretty: true,
 });
 
-const config = [
-  {
-    locale: { key: "zh_cn", htmlLang: "zh-cmn-Hans", label: "中" },
-    targetFileName: "index.html",
-    name: "赵 越",
-    desc: `进击的普通人.<b>123321312</b>`,
-    contact: {
-      title: "CONTACT",
-      list: [
-        { icon: "mailbox", href: "mailto:i@yuezhao.me", label: "i@yuezhao.me" },
-        { icon: "telegram", href: "", label: "@varzy", target: "_blank" },
-      ],
-    },
-    more: {
-      title: "MORE",
-      list: [{ label: "" }],
-    },
-  },
-  {
-    locale: { key: "en", htmlLang: "en", label: "ENG" },
-    targetFileName: "en.html",
-    name: "AIDEN ZHAO",
-    desc: `Test test.<b>123321312</b>`,
-    contact: {
-      title: "CONTACT",
-      list: [
-        { icon: "mailbox", href: "mailto:i@yuezhao.me", label: "i@yuezhao.me" },
-        { icon: "telegram", href: "", label: "@varzy", target: "_blank" },
-      ],
-    },
-    more: {
-      title: "MORE",
-      list: [{ label: "" }],
-    },
-  },
-];
-
-config.forEach((target) => {
+locales.forEach((locale) => {
   fs.writeFileSync(
-    path.resolve(__dirname, "./templates/" + target.targetFileName),
-    renderFn(target)
+    path.resolve(__dirname, './templates/' + locale.targetFileName),
+    renderFn(locale)
   );
 });
---
+++ src/locales.js
@@ -0,0 +1,68 @@
+module.exports = [
+  {
+    locale: { key: 'zh_cn', htmlLang: 'zh-cmn-Hans' },
+    targetFileName: 'index.html',
+    name: '赵 越',
+    descriptions: [`进击的普通人.<b>123321312</b>`, `12312312`],
+    contact: {
+      title: 'Contact',
+      list: [
+        { icon: 'fas fa-inbox', href: 'mailto:i@yuezhao.me', label: 'i@yuezhao.me' },
+        {
+          icon: 'fab fa-telegram-plane',
+          href: 'https://t.me/varzy',
+          label: '@varzy',
+          target: '_blank',
+        },
+      ],
+    },
+    links: {
+      title: 'Links',
+      list: [
+        { icon: 'fab fa-github', label: 'Github', href: 'https://github.com/varzy' },
+        { icon: 'fas fa-blog', label: '博客', href: 'https://varzy.me/blog/' },
+        { icon: 'fas fa-user-astronaut', label: '简历', href: 'https://varzy.me/resume/' },
+        { icon: 'fab fa-twitter', label: 'Twitter', href: 'https://twitter.com/varzyme' },
+        {
+          icon: 'fas fa-film',
+          label: '豆瓣',
+          href: 'https://www.douban.com/people/179641263/',
+          target: '_blank',
+        },
+      ],
+    },
+  },
+  {
+    locale: { key: 'en', htmlLang: 'en' },
+    targetFileName: 'en.html',
+    name: 'AIDEN ZHAO',
+    descriptions: [`Test test.<b>123321312</b>`, `12312312`],
+    contact: {
+      title: 'Contact',
+      list: [
+        { icon: 'fas fa-inbox', href: 'mailto:i@yuezhao.me', label: 'i@yuezhao.me' },
+        {
+          icon: 'fab fa-telegram-plane',
+          href: 'https://t.me/varzy',
+          label: '@varzy',
+          target: '_blank',
+        },
+      ],
+    },
+    links: {
+      title: 'Links',
+      list: [
+        { icon: 'fab fa-github', label: 'Github', href: 'https://github.com/varzy' },
+        { icon: 'fas fa-blog', label: 'Blog', href: 'https://varzy.me/blog/' },
+        { icon: 'fas fa-user-astronaut', label: 'Resume', href: 'https://varzy.me/resume/' },
+        { icon: 'fab fa-twitter', label: 'Twitter', href: 'https://twitter.com/varzyme' },
+        {
+          icon: 'fas fa-film',
+          label: 'Douban',
+          href: 'https://www.douban.com/people/179641263/',
+          target: '_blank',
+        },
+      ],
+    },
+  },
+];
