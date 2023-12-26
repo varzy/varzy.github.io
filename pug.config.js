ec67f7c52f9316b02e45d32601af1f7445c0fe6b
zy <varzy@live.com>
2023-12-26 11:23:15 +0800

    Update.

--- pug.config.js
+++ pug.config.js
@@ -23,28 +23,28 @@ module.exports = {
       name: '赵 越',
       descriptions: [
         `90 后程序员，现居北京。ACG 爱好者，老摇滚爱好者，半个极客。断舍离主义者，喜欢简洁的桌面和无 LOGO 的衣服。`,
-        `我的${cnLinks.blog}有几篇长文，那里是思绪的孤岛，欢迎登陆。我的 Telegram 频道「${cnLinks.telegramChannel}」是朋友圈，也会不时分享一些戳我的书影音，欢迎订阅。`,
+        `我的${cnLinks.blog}有几篇长文，那里是思绪的孤岛，欢迎登陆。我的 Telegram 频道「${cnLinks.telegramChannel}」是朋友圈，欢迎订阅。你还可以在${cnLinks.douban}翻一翻我标记的书影音，或者去${cnLinks.neteaseMusic}听一听我喜欢的音乐。`,
         `我对各种互联网技术都充满好奇，但当下更专注于 Web 前端领域。我使用 ${cnLinks.github} 托管开源项目。如果你正巧在招贤纳士，不妨瞄一眼我的${cnLinks.resume}。`,
         `尽管平平无奇，但仍然希望这个世界的运行轨迹能因我而发生一丝偏转。我正在努力。`,
       ],
-      links: [
-        {
-          title: '生活社交',
-          items: [cnLinks.instagram, cnLinks.neteaseMusic, cnLinks.douban],
-        },
-        {
-          title: '内容创作',
-          items: [cnLinks.blog, cnLinks.telegramChannel],
-        },
-        {
-          title: '技术工作',
-          items: [cnLinks.github, cnLinks.notes, cnLinks.resume],
-        },
-        {
-          title: '联系我',
-          items: [cnLinks.telegram, cnLinks.email],
-        },
-      ],
+      // links: [
+      //   {
+      //     title: '生活社交',
+      //     items: [cnLinks.instagram, cnLinks.neteaseMusic, cnLinks.douban],
+      //   },
+      //   {
+      //     title: '内容创作',
+      //     items: [cnLinks.blog, cnLinks.telegramChannel],
+      //   },
+      //   {
+      //     title: '技术工作',
+      //     items: [cnLinks.github, cnLinks.notes, cnLinks.resume],
+      //   },
+      //   {
+      //     title: '联系我',
+      //     items: [cnLinks.telegram, cnLinks.email],
+      //   },
+      // ],
     },
     // Support multi languages...
     // en: {
